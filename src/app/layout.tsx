8c73048ac49ebaa5f0035961acaf105009a00915
Jakub Rafalski <contact@rafalski.dev>
2026-07-27 11:05:48 +0200

    feat: add shadcn/ui with base colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "DJ-platform",
@@ -12,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
