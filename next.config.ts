15bb274e33bee4bffa0449979490015c71c06feb
Jakub Rafalski <contact@rafalski.dev>
2026-08-01 12:47:47 +0200

    chore: disable React strict mode and allow external dev origins

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  reactStrictMode: false,
+  allowedDevOrigins: ["192.168.4.43"],
 };
 
 const withNextIntl = createNextIntlPlugin();
