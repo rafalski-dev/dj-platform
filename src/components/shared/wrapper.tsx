509b9715bb6b7d971ce0dcf7bb1e38d9c85a3f01
Jakub Rafalski <contact@rafalski.dev>
2026-08-03 17:51:16 +0200

    style: adjust  wrapper padding and width

--- src/components/shared/wrapper.tsx
+++ src/components/shared/wrapper.tsx
@@ -3,5 +3,5 @@ type Props = {
 };
 
 export function Wrapper({ children }: Props) {
-  return <div className="mx-auto w-full max-w-300 px-4">{children}</div>;
+  return <div className="mx-auto w-full max-w-7xl px-4 md:px-5 lg:px-7">{children}</div>;
 }
