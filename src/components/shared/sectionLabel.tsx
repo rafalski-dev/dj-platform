0bf4a76bc397dfeab1d164f14ebc9c939170ce3e
Jakub Rafalski <contact@rafalski.dev>
2026-08-11 08:05:00 +0200

    style(sectionLabel): change font size for desktop

--- src/components/shared/sectionLabel.tsx
+++ src/components/shared/sectionLabel.tsx
@@ -4,7 +4,7 @@ export function SectionLabel({ children }: SectionLabelProps) {
   return (
     <div className={"text-primary mb-5 flex items-center gap-3"}>
       <span className="from-primary block h-px w-8 bg-linear-to-r from-20% to-transparent"></span>
-      <span className="text-[12.5px] font-medium tracking-[3px] uppercase md:text-[13.5px]">
+      <span className="text-[12.5px] font-medium tracking-[3px] uppercase md:text-[13px]">
         {children}
       </span>
     </div>
