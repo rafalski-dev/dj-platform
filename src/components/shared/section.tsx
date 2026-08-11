2de5b74c40a287824791714124f5546e00827147
Jakub Rafalski <contact@rafalski.dev>
2026-08-11 10:31:50 +0200

    style(section): add border top to section

--- src/components/shared/section.tsx
+++ src/components/shared/section.tsx
@@ -3,7 +3,10 @@ import { SectionProps } from "@/types/shared";
 
 export function Section({ children, className, ...props }: SectionProps) {
   return (
-    <section className={cn("w-full py-16 md:py-24 lg:py-32", className)} {...props}>
+    <section
+      className={cn("border-border/20 w-full border-t py-16 md:py-24 lg:py-32", className)}
+      {...props}
+    >
       {children}
     </section>
   );
