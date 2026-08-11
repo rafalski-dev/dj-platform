e7a6c03549a85f0b6db3fca3cda9c07f3e36bc1c
Jakub Rafalski <contact@rafalski.dev>
2026-08-11 06:56:19 +0200

    style(wrapper): add height property

--- src/components/shared/wrapper.tsx
+++ src/components/shared/wrapper.tsx
@@ -1,5 +1,5 @@
 import { WrapperProps } from "@/types/shared";
 
 export function Wrapper({ children }: WrapperProps) {
-  return <div className="mx-auto w-full max-w-7xl px-5 md:px-6 lg:px-8">{children}</div>;
+  return <div className="mx-auto h-full w-full max-w-7xl px-5 md:px-6 lg:px-8">{children}</div>;
 }
