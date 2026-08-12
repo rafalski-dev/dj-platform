cb79ca7cc2dca470fc2b93ecb5003a6d0483e3f6
Jakub Rafalski <contact@rafalski.dev>
2026-08-12 07:18:16 +0200

    feat(sectionTitle): add responsive font size at lg breakpoint

--- src/components/shared/sectionTitle.tsx
+++ src/components/shared/sectionTitle.tsx
@@ -3,6 +3,10 @@ import { SectionTitleProps } from "@/types/shared";
 
 export function SectionTitle({ children, className }: SectionTitleProps) {
   return (
-    <h2 className={cn("mb-6 text-[34px]/13 md:mb-8 md:text-[38px]/13", className)}>{children}</h2>
+    <h2
+      className={cn("mb-6 text-[34px]/13 md:mb-8 md:text-[38px]/13 lg:text-[54px]/18", className)}
+    >
+      {children}
+    </h2>
   );
 }
