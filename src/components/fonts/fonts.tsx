86ffc0afc04e1aaa144ec6f022cd43dda4278f39
Jakub Rafalski <contact@rafalski.dev>
2026-08-13 16:24:49 +0200

    refactor(fonts): simple object refactor

--- src/components/fonts/fonts.tsx
+++ src/components/fonts/fonts.tsx
@@ -1,6 +1,10 @@
 import { Cormorant_Garamond, Manrope } from "next/font/google";
 
-export const manrope = Manrope({ subsets: ["latin", "latin-ext"], variable: "--font-manrope" });
+export const manrope = Manrope({
+  subsets: ["latin", "latin-ext"],
+  variable: "--font-manrope",
+});
+
 export const cormorant = Cormorant_Garamond({
   subsets: ["latin", "latin-ext"],
   variable: "--font-cormorant",
