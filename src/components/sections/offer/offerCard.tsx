7da3cead30498a56bcf6d318ec88f67e606ecfc4
Jakub Rafalski <contact@rafalski.dev>
2026-08-13 10:25:44 +0200

    feat(offer): add responsive layout for breakpoints lg and xl

--- src/components/sections/offer/offerCard.tsx
+++ src/components/sections/offer/offerCard.tsx
@@ -10,33 +10,31 @@ export async function OfferCard({ cardNameKey, mostPopular, featuresList }: Offe
   const t = await getTranslations("LandingPage.Offer");
 
   return (
-    <li>
-      <article
-        className={clsx(
-          "bg-card relative flex flex-col gap-6 rounded-4xl border px-6 py-8 md:p-6",
-          mostPopular ? "border-accent/65 shadow-accent/50 shadow-xl/30" : "border-border/20",
-        )}
-      >
-        {mostPopular && (
-          <span className="bg-gold-gradient text-background shadow-accent/50 absolute top-0 left-[50%] flex h-7.5 -translate-x-1/2 -translate-y-1/2 items-center rounded-md px-4 text-[12.5px] font-semibold tracking-wider uppercase shadow-lg/50">
-            {t("mostPopular")}
-          </span>
-        )}
-        <header>
-          <h3 className="text-primary mb-2 text-[32px]">{t(`cards.${cardNameKey}.title`)}</h3>
-          <p className="text-popover-foreground text-[14px] font-light">
-            {t(`cards.${cardNameKey}.description`)}
-          </p>
-        </header>
-        <OfferCardFeatureList cardNameKey={cardNameKey} featuresList={featuresList} />
-        <footer>
-          <Button
-            variant="outline"
-            className="w-full"
-            render={<Link href="#contact">{t("primaryButton")}</Link>}
-          ></Button>
-        </footer>
-      </article>
-    </li>
+    <article
+      className={clsx(
+        "bg-card relative flex flex-col gap-6 rounded-4xl border px-6 py-8 md:p-6",
+        mostPopular ? "border-accent/65 shadow-accent/50 shadow-xl/30" : "border-border/20",
+      )}
+    >
+      {mostPopular && (
+        <span className="bg-gold-gradient text-background shadow-accent/50 absolute top-0 left-[50%] flex h-7.5 -translate-x-1/2 -translate-y-1/2 items-center rounded-md px-4 text-[12.5px] font-semibold tracking-wider uppercase shadow-lg/50">
+          {t("mostPopular")}
+        </span>
+      )}
+      <header>
+        <h3 className="text-primary mb-2 text-[32px]">{t(`cards.${cardNameKey}.title`)}</h3>
+        <p className="text-popover-foreground text-[14px] font-light">
+          {t(`cards.${cardNameKey}.description`)}
+        </p>
+      </header>
+      <OfferCardFeatureList cardNameKey={cardNameKey} featuresList={featuresList} />
+      <footer>
+        <Button
+          variant="outline"
+          className="w-full"
+          render={<Link href="#contact">{t("primaryButton")}</Link>}
+        ></Button>
+      </footer>
+    </article>
   );
 }
--- src/components/sections/offer/offerCardsList.tsx
+++ src/components/sections/offer/offerCardsList.tsx
@@ -3,15 +3,16 @@ import { OfferCard } from "./offerCard";
 
 export async function OfferCardsList({ cardsList }: { cardsList: OfferCardsDataType[] }) {
   return (
-    <ul className="flex flex-col gap-12">
+    <ul className="flex flex-col gap-12 lg:flex-row lg:flex-wrap lg:justify-center lg:gap-x-8 xl:grid xl:grid-cols-3">
       {cardsList.map(({ cardNameKey, mostPopular, featuresList }) => {
         return (
-          <OfferCard
-            key={cardNameKey}
-            cardNameKey={cardNameKey}
-            mostPopular={mostPopular}
-            featuresList={featuresList}
-          />
+          <li key={cardNameKey} className="lg:w-[40%] xl:w-full">
+            <OfferCard
+              cardNameKey={cardNameKey}
+              mostPopular={mostPopular}
+              featuresList={featuresList}
+            />
+          </li>
         );
       })}
     </ul>
