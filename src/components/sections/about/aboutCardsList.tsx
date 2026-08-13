8c57e1e4fb5a5f16eb3c30c4128c8f626f0de88e
Jakub Rafalski <contact@rafalski.dev>
2026-08-13 10:31:45 +0200

    style(about): update padding for cards and breakpoints

--- src/components/sections/about/aboutCardsList.tsx
+++ src/components/sections/about/aboutCardsList.tsx
@@ -3,11 +3,11 @@ import { getTranslations } from "next-intl/server";
 export async function AboutCardsList({ itemsList }: { itemsList: string[] }) {
   const t = await getTranslations("LandingPage.About");
   return (
-    <div className="grid grid-cols-2 gap-3.5 md:gap-4.5 lg:grid-cols-4">
+    <div className="grid grid-cols-2 gap-4 md:gap-5 lg:grid-cols-4">
       {itemsList.map((card) => (
         <div
           key={card}
-          className="border-border/20 bg-secondary flex min-h-42 flex-col justify-between gap-4 rounded-4xl border p-7 backdrop-blur-sm"
+          className="border-border/20 bg-secondary flex min-h-42 flex-col justify-between gap-4 rounded-4xl border px-6 py-5 backdrop-blur-sm md:py-6"
         >
           <span className="text-accent-foreground font-serif text-[36px] md:text-[48px]">
             {t(`stats.${card}.number`)}
