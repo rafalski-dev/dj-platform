56455e2a1986b74361c0e64de77b2643dbc685ba
Jakub Rafalski <contact@rafalski.dev>
2026-08-18 10:55:13 +0200

    feat(sectionLabel): add boolean prop to toggle line decoration

--- src/components/shared/sectionLabel.tsx
+++ src/components/shared/sectionLabel.tsx
@@ -1,11 +1,18 @@
 import { cn } from "@/lib/utils";
 import { SectionLabelProps } from "@/types/sharedComponents";
 
-export function SectionLabel({ children, textStyle, position = "left" }: SectionLabelProps) {
+export function SectionLabel({
+  children,
+  textStyle,
+  position = "left",
+  decoration = true,
+}: SectionLabelProps) {
   if (position === "center") {
     return (
       <div className={"text-primary mb-5 flex items-center gap-3"}>
-        <span className="from-primary block h-px w-8 bg-linear-to-l from-20% to-transparent"></span>
+        {decoration && (
+          <span className="from-primary block h-px w-8 bg-linear-to-l from-20% to-transparent"></span>
+        )}
         <p
           className={cn(
             "text-[12.5px] font-medium tracking-[3px] uppercase md:text-[13px]",
@@ -14,14 +21,18 @@ export function SectionLabel({ children, textStyle, position = "left" }: Section
         >
           {children}
         </p>
-        <span className="from-primary block h-px w-8 bg-linear-to-r from-20% to-transparent"></span>
+        {decoration && (
+          <span className="from-primary block h-px w-8 bg-linear-to-r from-20% to-transparent"></span>
+        )}
       </div>
     );
   }
 
   return (
     <div className={"text-primary mb-5 flex items-center gap-3"}>
-      <span className="from-primary block h-px w-8 bg-linear-to-r from-20% to-transparent"></span>
+      {decoration && (
+        <span className="from-primary block h-px w-8 bg-linear-to-r from-20% to-transparent"></span>
+      )}
       <p
         className={cn(
           "text-[12.5px] font-medium tracking-[3px] uppercase md:text-[13px]",
