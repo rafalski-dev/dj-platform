0ad8bc1ccf7860a325f48a57071c82f4fcb0b330
Jakub Rafalski <contact@rafalski.dev>
2026-08-24 10:13:49 +0200

    feat(ui): add shadcn components styled to match project design

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Input as InputPrimitive } from "@base-ui/react/input";
+
+import { cn } from "@/lib/utils";
+
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  return (
+    <InputPrimitive
+      type={type}
+      data-slot="input"
+      className={cn(
+        "border-input/20 file:text-foreground placeholder:text-muted-foreground/60 focus-visible:border-ring/80 focus-visible:ring-ring/40 aria-invalid:border-destructive/70 aria-invalid:ring-destructive/30 dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 bg-background h-12 w-full min-w-0 rounded-md border px-2.5 py-1 text-[14.5px] shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-3 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-3 md:text-sm",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Input };
