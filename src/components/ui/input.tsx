2be774cb9bff033c5b74d5590e9d346e2dda563b
Jakub Rafalski <contact@rafalski.dev>
2026-08-26 07:37:16 +0200

    style(form): adjust form components styles to match project design

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "border-input/20 file:text-foreground placeholder:text-muted-foreground/60 focus-visible:border-ring/80 focus-visible:ring-ring/40 aria-invalid:border-destructive/70 aria-invalid:ring-destructive/30 dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 bg-background h-12 w-full min-w-0 rounded-md border px-2.5 py-1 text-[14.5px] shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-3 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-3 md:text-sm",
+        "border-input/15 file:text-foreground placeholder:text-muted-foreground/60 focus-visible:border-ring/80 focus-visible:ring-ring/40 aria-invalid:border-destructive/70 aria-invalid:ring-destructive/20 dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 bg-background h-11 w-full min-w-0 rounded-md border px-3.5 py-1 text-sm shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-3 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-3",
         className,
       )}
       {...props}
