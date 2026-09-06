a286553b6099e0e25db94eaf758c61612c3d9a00
Jakub Rafalski <contact@rafalski.dev>
2026-09-06 08:58:12 +0200

    fix(decoration): add minus z-index to move glow orb under the main layout

--- src/components/shared/decorations/glowOrbs.tsx
+++ src/components/shared/decorations/glowOrbs.tsx
@@ -24,7 +24,7 @@ export function BgGlowOrb({
 }: BgGlowOrbProps) {
   return (
     <div
-      className={cn("absolute rounded-full", position, className)}
+      className={cn("absolute -z-10 rounded-full", position, className)}
       style={{
         width: `${size}vw`,
         height: `${size}vw`,
@@ -53,7 +53,7 @@ export function FloatingGlowOrb({
 }: FloatingGlowOrbProps) {
   return (
     <div
-      className={cn("absolute z-[-5] rounded-full", className, display)}
+      className={cn("absolute -z-20 rounded-full", className, display)}
       style={
         {
           top: "0",
