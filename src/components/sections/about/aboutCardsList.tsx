2011c483133d4d3f3e661f38dc6f293c1611afa9
Jakub Rafalski <contact@rafalski.dev>
2026-09-06 09:03:43 +0200

    style(about): change font colors

--- src/components/sections/about/aboutCardsList.tsx
+++ src/components/sections/about/aboutCardsList.tsx
@@ -9,10 +9,10 @@ export async function AboutCardsList({ itemsList }: { itemsList: string[] }) {
           key={card}
           className="border-border/12 bg-secondary flex min-h-42 flex-col justify-between gap-4 rounded-4xl border px-6 py-5 backdrop-blur-sm md:py-6"
         >
-          <span className="text-accent-foreground font-serif text-[36px] md:text-[48px]">
+          <span className="text-accent font-serif text-[36px] md:text-[48px]">
             {t(`stats.${card}.number`)}
           </span>
-          <span className="text-muted-foreground/90 text-[14px] font-light md:text-[15px]">
+          <span className="text-popover-foreground text-[14px] font-light md:text-[15px]">
             {t(`stats.${card}.label`)}
           </span>
         </div>
