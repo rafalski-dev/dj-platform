f0d3adc415200a9c34a1eeeb9466c9424ec026de
Jakub Rafalski <contact@rafalski.dev>
2026-09-07 08:18:53 +0200

    style(footer): change hover color

--- src/components/shared/footer/footerLink.tsx
+++ src/components/shared/footer/footerLink.tsx
@@ -4,7 +4,7 @@ import { FooterLinkProps } from "@/types/footer";
 export function FooterLink({ navKey, path }: FooterLinkProps) {
   const isAnchor = path.startsWith("#");
   const className =
-    "text-popover-foreground hover:text-accent-foreground text-[14.5px] font-normal transition-colors lg:text-base";
+    "text-popover-foreground hover:text-accent-foreground/90 text-[14.5px] font-normal transition-colors lg:text-base";
 
   if (isAnchor)
     return (
