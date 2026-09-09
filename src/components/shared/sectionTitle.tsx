5bcd30f0836a1b2ddc936958fa3553371fb36d9d
Jakub Rafalski <contact@rafalski.dev>
2026-09-09 16:33:01 +0200

    feat(sectionTitle): add animation props

--- src/components/shared/sectionTitle.tsx
+++ src/components/shared/sectionTitle.tsx
@@ -1,10 +1,11 @@
 import { cn } from "@/lib/utils";
 import { SectionTitleProps } from "@/types/sharedComponents";
 
-export function SectionTitle({ children, className }: SectionTitleProps) {
+export function SectionTitle({ children, className, animation }: SectionTitleProps) {
   return (
     <h2
       className={cn("mb-5 text-[34px]/11 md:mb-6 md:text-[38px]/12 lg:text-[54px]/16", className)}
+      style={{ animation: animation }}
     >
       {children}
     </h2>
